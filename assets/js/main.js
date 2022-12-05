fb3c34b25094f0faa5a8b5e28631d095de3c5378
Adam Lacerda <107153882+adamlacerda@users.noreply.github.com>
2022-12-05 14:48:13 -0300

    team-map

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,3 +46,25 @@ $(".portfolio-slider").owlCarousel({
         }
     }
 })
+
+// Review Slider
+
+$(".reviews-slider").owlCarousel({
+    loop: true,
+    nav: true,
+    dots: false,
+    smartSpeed: 1000,
+    margin: 24,
+    navText: ["<i class='ri-arrow-left-line'></i>", "<i class='ri-arrow-right-line'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        992:{
+            items:2
+        }
+    }
+})
